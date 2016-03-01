674128a6504ebb2a4e2e23f5da9cc003cb4b3b00
franzi <franziska-saul@gmx.de>
2016-03-01 16:07:34 +0100

    read_fasta-datei_in_Modul_read_fasta

--- readfasta.pl
+++ readfasta.pl
@@ -1,35 +1,13 @@
 use warnings;
 use strict;
+use read_fasta;
 
-my @elements;
-my %fasta;
-my $seq="";
 my $datei = "fastatest.fasta";
-open (IN, $datei) or die;
-while(<IN>)
-{
-    chomp;
-    my $zeile = $_;
-    if($zeile =~/>/)
-    {
-	@elements = split(/ /,$zeile);
-	$seq = "";
-    }
-    else
-    {
-	$seq = $seq.$_;
-    }
-    $fasta{$elements[0]} = {species=>$elements[1],
-			    seq=>$seq};
-    #print "@elements\n";
-    #print "$seq\n";
-}
-close IN;
-
-foreach my $bla (keys %fasta)
+my %output = read_fasta::read_fasta($datei);
+foreach my $bla (keys %output)
 {
     print "$bla\n";
-    print "$fasta{$bla}->{species}\n";
-    print "$fasta{$bla}->{seq}\n \n";
+    print "$output{$bla}->{species}\n";
+    print "$output{$bla}->{seq}\n \n";
 }
 
