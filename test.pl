7b28746db6fb2f8ece10438ff0329afe5207136e
franzi <franziska-saul@gmx.de>
2016-03-02 11:38:52 +0100

    use_Test::More_eingefuegt

--- test.pl
+++ test.pl
@@ -1,32 +1,40 @@
 use warnings;
 use strict;
 use read_fasta;
+use Test::More tests=>2;
 
 my $datei = "fastatest.fasta";
 my %output = read_fasta::read_fasta($datei);
+my $wert1 = $output{">ID1"}->{species};
+my $wert2 = $output{">ID1"}->{seq};
+my $expected1 = "Mensch";
+my $expected2 = "CTAFGTILV";
 
-if ($output{">ID1"}->{species} eq "Mensch")
-{
-    print "species funktioniert\n";
-    if ($output{">ID1"}->{seq} eq "CTAFGTILV")
-    {
-	print "sequence funktioniert\n";
-    }
-    else
-    {
-        print "sequence funktioniert nicht\n";
-    }
-}
-else
-{
-    print "species funktioniert nicht\n";
-    if ($output{">ID1"}->{seq} eq "CTAFGTILV")
-    {
-	print "sequence funktioniert\n";
-    }
-    else
-    {
-        print "sequence funktioniert nicht\n";
-    }
-}
+is($wert1, $expected1, my $test_species);
+is($wert2, $expected2, my $test_sequence);
+
+# if ($output{">ID1"}->{species} eq "Mensch")
+# {
+#     print "species funktioniert\n";
+#     if ($output{">ID1"}->{seq} eq "CTAFGTILV")
+#     {
+# 	print "sequence funktioniert\n";
+#     }
+#     else
+#     {
+#         print "sequence funktioniert nicht\n";
+#     }
+# }
+# else
+# {
+#     print "species funktioniert nicht\n";
+#     if ($output{">ID1"}->{seq} eq "CTAFGTILV")
+#     {
+# 	print "sequence funktioniert\n";
+#     }
+#     else
+#     {
+#         print "sequence funktioniert nicht\n";
+#     }
+# }
 
