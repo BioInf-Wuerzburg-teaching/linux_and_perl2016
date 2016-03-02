069e130c60bee984dfdefca5d783e5770b88a499
Aaron Sigmund <j.reins@thidi.de>
2016-03-02 11:20:02 +0100

    Removed print output. Added Data::Dumper output.

--- Tag-3-Testbefehle-aaron.pl
+++ Tag-3-Testbefehle-aaron.pl
@@ -2,33 +2,27 @@ use warnings;
 use strict;
 use fpr;
 use Term::ANSIColor;
+use Data::Dumper;
 
 my $datei="fanta.fasta";
 my %input = fpr::fasta_parser($datei);
-my $key=">ID2";
+my $key=">ID3";
 my $result;
 
-foreach my $tmp (keys %input) #$tmp als zwischenspeicher
+foreach my $tmp (keys %input) 
 {
 	$result = "$tmp\n$input{$tmp}->{species}\n$input{$tmp}->{sequence}\n \n";
 
 }
 
-#Geklauter Code, geht nicht bisher...
-#    print "Exists\n"    if exists $input{$key};
-#    print "Defined\n"   if defined $input{$key};
-#    print "True\n"      if $input{$key};
-
 #Testroutine
 if($input{$key}->{species} eq "Maus")
 {
 	print color("green"), "\nTest successful. Congrats!\n\n", color("reset");
-	print "Result:\n\n";
-	print "$result\n\n";
+	print Dumper(\%input);
 }
 else
 {
 	print color("red"), "\nTest failed. You may get a coffee and look through your code.\n\n", color("reset");
-	print "Result:\n\n";
-	print "$result\n\n";	
+	print Dumper(\%input);	
 }
