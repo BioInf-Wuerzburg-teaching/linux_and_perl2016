7387a09c8bca2cbbb91c6d6fc225734e1eacfc96
SimonP <Van91@gmx.de>
2016-03-02 10:49:51 +0100

    Test improved, tests now more!

--- test_S.pl
+++ test_S.pl
@@ -3,24 +3,28 @@ use warnings;
 use strict;
 use fasta_modul;
 
-my $datei = "fasta.fa";
+my $datei = "fasta.fa";  #laden der Datei
 
-my %fastatest=fasta_modul::fasta_parser($datei);
+my %fastatest=fasta_modul::fasta_parser($datei); #übergeben an sub
 
-if($fastatest{">seq3"}{"spec"} eq "Maus"){
+
+#Test 1 Species
+
+if($fastatest{">seq3"}{"spec"} eq "Maus" && $fastatest{">seq1"}{"spec"} eq "Mensch"){
     print "Test bestanden!(Spec)\n";
 }
 else{
     print "Sechs setzen!\n";
 };
 
+#Test 2 Sequence
 
-if($fastatest{">seq2"}{"seq"} eq "CTGAGTTTTT"){
+if($fastatest{">seq2"}{"seq"} eq "CTGAGTTTTT" && $fastatest{">seq1"}{"seq"} eq "CTAGTGTTTT"){
     print "Test2 bestanden!(seq)\n"
 }
 else {
     print "Sechs setzen!\n";
-}
+};
 
 
 
