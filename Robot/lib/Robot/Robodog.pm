ce390233a248c53e960ddfb0beb296b271eb7ed4
Andi <andreas.pittroff@stud-mail.uni-wuerzburg.de>
2016-03-03 18:22:37 +0100

    changed sub new to SUPERCOOL code

--- Robot/lib/Robot/Robodog.pm
+++ Robot/lib/Robot/Robodog.pm
@@ -3,14 +3,13 @@ use strict;
 use warnings;
 
 use parent qw(Robot);
-#use Robot;
-#our @ISA=qw(Robot);
+
 
 sub new
 {
-    my $self={};
-    bless $self, 'Robot::Robodog';
-    $self= $self->SUPER::new();
+    my $class=shift;
+    $class=ref($class)||$class;
+    my $self=$class->SUPER::new();
     $self->{tail}='low';
     return $self;
 }
