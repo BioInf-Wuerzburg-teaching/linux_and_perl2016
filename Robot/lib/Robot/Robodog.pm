f01d81db8b5964e2f6db3b4a7699b82191bb866c
Andi <andreas.pittroff@stud-mail.uni-wuerzburg.de>
2016-03-03 18:16:13 +0100

    completed sub new for Robodog

--- Robot/lib/Robot/Robodog.pm
+++ Robot/lib/Robot/Robodog.pm
@@ -3,8 +3,17 @@ use strict;
 use warnings;
 
 use parent qw(Robot);
+#use Robot;
+#our @ISA=qw(Robot);
 
-sub new{}
+sub new
+{
+    my $self={};
+    bless $self, 'Robot::Robodog';
+    $self= $self->SUPER::new();
+    $self->{tail}='low';
+    return $self;
+}
 
 
 1;
