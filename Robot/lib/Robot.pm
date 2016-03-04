b4bffc0e7c17a23011ec3c0bb81a70758476293b
Andi <andreas.pittroff@stud-mail.uni-wuerzburg.de>
2016-03-04 09:57:13 +0100

    added POD to Robot.pm

--- Robot/lib/Robot.pm
+++ Robot/lib/Robot.pm
@@ -1,5 +1,9 @@
 package Robot;
+=head1 Robot
 
+Dieses  Programm baut extrem coole Roboter, die ueber die Faehigkeit verfuegen sich und ihre Arme zu bewegen.Zudem sind sie in der Lage ihre eigene Position, sowie die Position ihrer Arme zurueckzugeben.
+
+=cut
 use 5.010001;
 use strict;
 use warnings;
@@ -8,7 +12,13 @@ our @ISA = qw();
 
 our $VERSION = '0.1';
 
+=head1 new()
 
+Diese Subroutine baut einen extrem coolen Roboter
+    $var=Robot::new();
+Der Roboter wird auf der Position 0|0 erschaffen und verfuegt ueber zwei Arme, die beide nach unten gerichtet sind.
+
+=cut
 sub new
 {
     my $class=shift;
@@ -22,7 +32,12 @@ sub new
     bless $robot, $class;
     return $robot;
 }
+=head1 move()
 
+Diese Subroutine bewegt den Roboter auf die Koordinaten x,y.
+    $robot->move($x,$y);
+
+=cut
 sub move
 {
     my $self = shift;
@@ -30,6 +45,12 @@ sub move
     $self->{y}=$_[1];
     return $self->pos();
 }
+=head1 pos()
+
+Diese Subroutine laesst den Roboter seine aktuelle Position im Koordinatensystem ausgeben.
+    $robot->pos();
+
+=cut
 sub pos
 {
     my $self = shift;
@@ -37,20 +58,35 @@ sub pos
     return @pos;
     
 }
+=head1 links()
 
+Diese Subroutine laesst den Roboter die Position seines linken Armes ausgeben.
+    $robot->links();
+
+=cut
 sub links
 {
     my $self=shift;
     return $self->{links};
 }
+=head1 rechts()
+
+Diese Subroutine laesst den Roboter die Position seines rechten Armes ausgeben.
+    $robot->rechts();
+
+=cut
 sub rechts
 {
     my $robot=$_[0];
     my $pos_rechts=$robot->{'rechts'};
     return $pos_rechts;
 }
+=head1 links_switch()
 
+Diese Subroutine laesst den Roboter die Position seines linken Armes wechseln.
+    $robot->links_switch();
 
+=cut
 sub links_switch{
     my $self=shift;
     if($self->{links} eq "unten")
@@ -62,59 +98,23 @@ sub links_switch{
 	$self->{links}="unten";
     }
 }
+=head1 rechts_switch()
 
+Diese Subroutine  laesst den Roboter seinen rechten Arm nach oben nehmen.
+    $robot->rechts_switch();
 
-
+=cut
 sub rechts_switch
 {
     my $robot=$_[0];
     $robot->{'rechts'} ='oben';
     return $robot->rechts();
 }
-1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
+=head1 AUTOR
 
-=head1 NAME
-
-Robot - Perl extension for blah blah blah
-
-=head1 SYNOPSIS
-
-  use Robot;
-  blah blah blah
-
-=head1 DESCRIPTION
-
-Stub documentation for Robot, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
-=head1 AUTHOR
-
-A. U. Thor, E<lt>biokurs@E<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2016 by A. U. Thor
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.18.2 or,
-at your option, any later version of Perl 5 you may have available.
-
+Linux_und_Perl-Gefangene 2016
 
 =cut
+1;
+__END__
+
