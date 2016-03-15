b43715b0be888cfcbb9de230c34e8752250ad993
Tillsa <till.sauerwein@web.de>
2016-03-15 17:53:13 +0100

    programm count_pm, dass auf die subroutine 'put_zeile_into_array' zugreift, um mutationen zwischen zwei sequenzen zu untersuchen

---
+++ Fasta/count_pm.pl
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+use lib "./lib/";
+use Fasta;
+
+my $file= "rosalind_hamm.txt";
+
+my $parser= Fasta->new();
+
+$parser->filename($file);
+
+
+my $num_mismatch=$parser->put_zeile_into_array();
+print "$num_mismatch\n";
+
+
+#my $string1 = $beidezeilen[0];
+#my $string2 = $beidezeilen[1];
+#my @string1=split('', $string1);
+#my @string2=split('', $string2);
+#
+#my $num_mismatch = 0;
+#my $len = length($string1);
+#for (my $i=0; $i<$len; $i++){
+#    ++$num_mismatch if substr($string1, $i, 1) ne substr($string2, $i, 1);
+#}
+#print $num_mismatch;
+
